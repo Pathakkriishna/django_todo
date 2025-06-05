147765eee14b9a4cf49c3b143b64a5e92e560e1c
Krishna Pathak <kriishna.pathak@gmail.com>
2025-06-05 07:34:17 +0545

    type added

--- base/models.py
+++ base/models.py
@@ -7,3 +7,7 @@ class Todo(models.Model):
     name = models.CharField(max_length=300)
     description = models.TextField()
     status = models.CharField(max_length=300)
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=300)
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo
+from .models import Type
 
 # Create your views here.
 
@@ -8,3 +9,8 @@ from .models import Todo
 def home(request):
     todos = Todo.objects.all()
     return render(request, 'index.html', context={'todos': todos})
+
+
+def type(request):
+    types = Type.objects.all()
+    return render(request, 'type.html', context={'types': types})
